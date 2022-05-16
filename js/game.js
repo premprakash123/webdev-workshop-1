dbcb20e4a31c59ca0d2515a2eef6939121904511
Prem Prakash Sahu <premprakash1617200@gmail.com>
2022-05-16 07:38:46 +0000

    my changes3

--- js/game.js
+++ js/game.js
@@ -5,6 +5,51 @@ const TENOR_API_KEY = ""; // you need to add your own API key from Tenor and rep
 
 function on_page_load() {
     console.log("Page has loaded ...");
+    fetch_a_question();
 }
+function on_question_fetched(question_text,correct_answer,incorrect_answers){
+    update_question_ui();
+    fetch_image();
+}
+function on_image_fetched(){
+    update_image_ui();
+}
+function fetch_a_question(){
+    console.log("Fetch a question...");
+    fetch(TRIVIA_API_URL)
+       .then(response => response.json())
+         .then(data => {
+             console.log(data);
+             const question_text = data["results"][0]["question"];
+             const correct_answer = data["results"][0]["correct_answer"];
+             const incorrect_answers = data["results"][0]["incorrect_answers"];
+             on_question_fetched(question_text,correct_answer,incorrect_answers);
+         });
+
+}
+function update_question_ui(question_text,correct_answer,incorrect_answers){
+     document.getElementById("question_text").innerHTML = question_text;
+     answer_buttons = getElementByClassName("answer_button");
+     const answer_options = incorrect_answers;
+     const insertion_index = Math.floor(Math.random * 4);
+     answer_options.splice(0,0,correct_answer);
+     for(let i = 0; i<4;i++){
+        answer_buttons[i].innerHTML = answer_options[i];
+     }
+}
+function fetch_image(correct_answer){
+    console.log("Fetch image...");
+    fetch(TENOR_API_URL)
+    .then(response => response.json())
+      .then(data => {
+          console.log(data);
+          const question_text = data["results"][0]["question"];
+          const correct_answer = data["results"][0]["correct_answer"];
+          const incorrect_answers = data["results"][0]["incorrect_answers"];
+          on_question_fetched(question_text,correct_answer,incorrect_answers);
+      });
+
+}
+
 
 window.onload = on_page_load;
